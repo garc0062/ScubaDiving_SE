4bdd5dfc4558fca70fdfaf7f7b4b1e62374cc638
JUAN\pablo <pablo.gg@portalink.com>
2018-03-30 22:10:50 +1030

    Backlog 3 and 7. Get attributes for calculations

--- ScubaDivingSE3/src/model/SimpleCalculation.java
+++ ScubaDivingSE3/src/model/SimpleCalculation.java
@@ -13,17 +13,51 @@ package model;
  * Manages simple calculations for scuba diving project.
  */
 public class SimpleCalculation {
-    
+
     //Range for PPO2 values
     public static final float MIN_PPO2_VALUE = 1.1F;
     public static final float MAX_PPO2_VALUE = 1.6F;
     //Range for Oxygen values
     public static final int MIN_OXYGEN_VALUE = 22;
     public static final int MAX_OXYGEN_VALUE = 50;
+    //Min value of depth
+    public static final int MIN_DEPTH_VALUE = 0;
 
     int oxigen;
     float ppo2;
-    int depth;
+    float depth;
+
+     /**
+     * Get oxygen in percentage
+     * @return oxygen in percentage
+     */
+    public int getOxigenInPercentage() {
+        return oxigen;
+    }
+
+     /**
+     * Get oxygen in decimal
+     * @return oxygen in decimal
+     */
+    public float getOxigenInDecimal() {
+        return (float)oxigen * 0.01F;
+    }
+
+    public float getPpo2() {
+        return ppo2;
+    }
+
+    public float getDepth() {
+        return depth;
+    }
+    
+    /**
+     * Converts and returns absolute pressure at depth from depth in meters
+     * @return oxygen in decimal
+     */
+    public float getAbsolutePressure() {
+        return (float)depth/10 + 1;
+    }
 
     /**
      * Set value of PPO2 between valid values range (1.1 and 1.6)
@@ -38,16 +72,32 @@ public class SimpleCalculation {
         }
         return false;
     }
-    
+
     /**
-     * Set value of Oxygen between valid values range (MIN_OXYGEN_VALUE and MAX_OXYGEN_VALUE)
+     * Set value of Oxygen between valid values range (MIN_OXYGEN_VALUE and
+     * MAX_OXYGEN_VALUE)
      *
-     * @param oxygen: partial pressure of oxygen to set.
+     * @param oxygen: percentage of oxygen in mixture to set.
      * @return true if value was properly set (within range). False otherwise.
      */
     public boolean setOxygen(int oxygen) {
         if (oxygen >= MIN_OXYGEN_VALUE && oxygen <= MAX_OXYGEN_VALUE) {
             this.oxigen = oxygen;
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Set value of Oxygen between valid values range (MIN_OXYGEN_VALUE and
+     * MAX_OXYGEN_VALUE)
+     *
+     * @param depth: partial pressure of oxygen to set.
+     * @return true if value was properly set (within range). False otherwise.
+     */
+    public boolean setDepth(float depth) {
+        if (depth >= 0) {
+            this.depth = depth;
             return true;
         }
         return false;
--- ScubaDivingSE3/test/model/SimpleCalculationTest.java
+++ ScubaDivingSE3/test/model/SimpleCalculationTest.java
@@ -58,7 +58,7 @@ public class SimpleCalculationTest {
     public void testSetOxygen() {
         System.out.println("setOxygen");
         SimpleCalculation instance = new SimpleCalculation();
-                //First Scenario: values outside of range
+        //First Scenario: values outside of range
         assertEquals(false, instance.setOxygen(-5));
         assertEquals(false, instance.setOxygen(10));
         assertEquals(false, instance.setOxygen(21));
@@ -72,6 +72,99 @@ public class SimpleCalculationTest {
         assertEquals(true, instance.setOxygen(31));
         assertEquals(true, instance.setOxygen(40));
         assertEquals(true, instance.setOxygen(48));
+    }
+
+    /**
+     * Test of setDepth method, of class SimpleCalculation.
+     */
+    @Test
+    public void testSetDepth() {
+        System.out.println("setDepth");
+        SimpleCalculation instance = new SimpleCalculation();
+        //Less than min
+        assertEquals(false, instance.setDepth(-1));
+        assertEquals(false, instance.setDepth(-5));
+        assertEquals(false, instance.setDepth(-155));
+        //More than or equal to zero
+        assertEquals(true, instance.setDepth(SimpleCalculation.MIN_DEPTH_VALUE));
+        assertEquals(true, instance.setDepth(10));
+        assertEquals(true, instance.setDepth(70));
+    }
+
+    /**
+     * Test of getOxigenInPercentage method, of class SimpleCalculation.
+     */
+    @Test
+    public void testGetOxigenInPercentage() {
+        System.out.println("getOxigenInPercentage");
+        SimpleCalculation instance = new SimpleCalculation();
+        //Set value and get value in parcentage should be equal
+        instance.setOxygen(23);
+        assertEquals(23, instance.getOxigenInPercentage());
+        instance.setOxygen(40);
+        assertEquals(40, instance.getOxigenInPercentage());
+        instance.setOxygen(50);
+        assertEquals(50, instance.getOxigenInPercentage());
+    }
+
+    /**
+     * Test of getOxigenInDecimal method, of class SimpleCalculation.
+     */
+    @Test
+    public void testGetOxigenInDecimal() {
+        System.out.println("getOxigenInDecimal");
+        SimpleCalculation instance = new SimpleCalculation();
+        //Get value in decimal should be the decimal of the percentage
+        instance.setOxygen(23);
+        assertEquals(0.23F, instance.getOxigenInDecimal(), 2);
+        instance.setOxygen(40);
+        assertEquals(0.40F, instance.getOxigenInDecimal(), 2);
+        instance.setOxygen(50);
+        assertEquals(0.50F, instance.getOxigenInDecimal(), 2);
+    }
+
+    /**
+     * Test of getPpo2 method, of class SimpleCalculation.
+     */
+    @Test
+    public void testGetPpo2() {
+        System.out.println("getPpo2");
+        SimpleCalculation instance = new SimpleCalculation();
+        //Set value and get value in parcentage should be equal
+        instance.setPPO2(1.1F);
+        assertEquals(1.1F, instance.getPpo2(), 1);
+        instance.setPPO2(1.3F);
+        assertEquals(1.3F, instance.getPpo2(), 1);
+    }
+
+    /**
+     * Test of getDepth method, of class SimpleCalculation.
+     */
+    @Test
+    public void testGetDepth() {
+        System.out.println("getDepth");
+        SimpleCalculation instance = new SimpleCalculation();
+        //Set value and get value in parcentage should be equal
+        instance.setDepth(10);
+        assertEquals(10, instance.getDepth(), 2);
+        instance.setDepth(30);
+        assertEquals(30, instance.getDepth(), 2);
+    }
+
+    /**
+     * Test of getAbsolutePressure method, of class SimpleCalculation.
+     */
+    @Test
+    public void testGetAbsolutePressure() {
+        System.out.println("getAbsolutePressure");
+        SimpleCalculation instance = new SimpleCalculation();
+        //Get value should be equal to applying formula
+        instance.setDepth(28.8F);
+        assertEquals(3.88F, instance.getAbsolutePressure(), 2);
+        instance.setDepth(30);
+        assertEquals(4F, instance.getAbsolutePressure(), 2);
+        instance.setDepth(24);
+        assertEquals(3.44F, instance.getAbsolutePressure(), 2);
     }
 
 }
